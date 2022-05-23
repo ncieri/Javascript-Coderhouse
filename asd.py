4a7f8398b23104f9e186577e14845ac11eba6308
Ncieri1 <nicocieri29@hotmail.com>
2022-05-23 17:40:36 -0300

    Se agrego la vinculacion al dom

--- asd.py
+++ asd.py
@@ -7,19 +7,19 @@ listaProductos = prompt("Hola, escribí PRODUCTO para ver los productos disponib
 const bebidas = [{
     bebida: "Coca-Cola",
     sabor: "cola",
-    precioProducto: 120
+    precioProducto: 360
 }, {
     bebida: "Fanta",
     sabor: "Naranja",
-    precioProducto: 130
+    precioProducto: 475
 }, {
     bebida: "Coca-Cola Zero",
     sabor: "cola",
-    precioProducto: 150
+    precioProducto: 350
 }, {
     bebida: "Sprite",
     sabor: "limón",
-    precioProducto: 120
+    precioProducto: 470
 }, ];
 
 bebidas.forEach((refresco) => {
@@ -34,32 +34,30 @@ while(listaProductos !== "PRODUCTO"){
 
 alert("Estos son nuestros productos disponibles :" +  "\n" +listaBebidasPrecios)
 
-let productos = prompt("Elige tu producto: \n 1. Coca-Cola \n 2. Fanta \n 3. Coca-Cola Zero \n 4. Sprite \n 5. Bebidas sugeridas \n Escribe ESC para finalizar la compra")
+let productos = prompt("Elige tu producto: \n 1. Coca-Cola \n 2. Fanta \n 3. Coca-Cola Zero \n 4. Sprite  \n Escribe ESC para finalizar la compra")
 
 function carrito() {
     switch (productos) {
         case "1":
             alert('Coca-Cola se agregó al carrito');
-            precioFinal(120)
+            precioFinal(360)
             break
 
         case "2":
             alert('Fanta se agregó al carrito');
-            precioFinal(130)
+            precioFinal(475)
             break
 
         case "3":
             alert('Coca-Cola Zero se agregó al carrito');
-            precioFinal(150)
+            precioFinal(350)
             break
 
         case "4":
             alert('Sprite se agregó al carrito');
-            precioFinal(120)
+            precioFinal(470)
             break
-        case "5":
-            bebidaSugerida()
-            break
+
         default:
             alert('Todavía no tenemos esa bebida en nuestra tienda ');
     }
@@ -73,29 +71,7 @@ function precioFinal(precio) {
 
 do {
     carrito()
-    productos = prompt("Elige tu producto: \n 1. Coca-Cola \n 2. Fanta \n 3. Coca-Cola Zero \n 4. Sprite \n 5. Bebidas deseadas \n Escribe ESC para finalizar la compra")
-} while (productos != 'ESC') 
+    productos = prompt("Elige tu producto: \n 1. Coca-Cola \n 2. Fanta \n 3. Coca-Cola Zero \n 4. Sprite  \n Escribe ESC para finalizar la compra")
+} while (productos != 'ESC') {
     alert('Compra finalizada. El precio total de su compra es : $' + precioTotal)
-
-
-
-
-    iniciarApp();
-
-function iniciarApp()
-{
-    const H1 = document.createElement("h1");
-        H1.innerHTML="TIENDA DE REFRESCOS";
-
-        H1.setAttribute("style","color:blue;text-align:center");
-        H1.classList.add("miTitulo");
-        H1.classList.add("miTitulo2");
-
-    document.body.appendChild(H1);
-
-    const subtitulo = document.createElement("p");
-    subtitulo.innerText="Listado de productos:";
-
-    document.body.appendChild(subtitulo);
 }
-
